edcaa2b92ee3f7e5b6a87847c055448b52b6d3a6
TikiTikiPlus <ro68@students.waikato.ac.nz>
2023-03-26 19:41:01 +1300

    Update sortFileName.py

--- scripts/sortFileName.py
+++ scripts/sortFileName.py
@@ -4,4 +4,4 @@ if os.path.isdir(folder):
     files = os.listdir(folder)
     files.sort(key=lambda x: (x.split('.')[0]))
     for file in files:
-        os.system("type " + folder + "\\" + file + ">> test.txt")
+        os.system("cat " + folder + "\\" + file + ">> test.txt")
